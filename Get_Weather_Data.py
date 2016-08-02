309232861b0726fcaa0e261454106cd3cc30413f
ShyamThiagarajan <shyam.thiagarajan@gmail.com>
2016-08-01 22:58:07 -0400

    Updated Method Doc and Started Gathering data

--- Get_Weather_Data.py
+++ Get_Weather_Data.py
@@ -3,33 +3,64 @@ from bs4 import BeautifulSoup as HTML
 import urllib2
 from Weather_Forecast import Weather_Forecast
 
+
+
+"""Gets all Coordinates to determine weather for
+@return lat_lng
+    Dictionary of lat: lng"""
+def getAllCoordinates():
+    lat_lng = {}
+    with open('lat_lng') as city_data_file:
+        for city_data in city_data_file:
+            city_data = city_data.split(',')
+            state = city_data[1]
+            city = city_data[2]
+            print city + ', ' + state
+            lat = city_data[3]
+            lng = city_data[4].strip('\n')
+            lat_lng[lat] = lng
+    return lat_lng
+
+
+"""Gets base url to parse
+@param lat
+    latitude to replace into url
+@param lng
+    Longitude to replace into url"""
 def getURL(lat, lng):
     config = ConfigParser.ConfigParser()
     config.read('CONFIG.ini')
     url = (config.get('URL', 'Knoxville'))
     url = str(url).replace('lat', str(lat)).replace('lng', str(lng))
-    print url
     return url
 
 
+"""Gets Weather Information and Writes data to file.
+@param url
+    url to parse weather for
+@param lat
+    latitude of weather forecast
+@param lng
+    longitude of weather forecast"""
 def getWeather(url, lat, lng):
     weather_data = HTML(urllib2.urlopen(url).read(), 'lxml')
     # Navigate down HTML to the weather table (line 410 generally)
-    weather_table = weather_data.body.contents[5].contents[5].contents[22].contents[5].contents[5].table
-    weather_forecast = Weather_Forecast(lat, lng)
-    weather_forecast.detWeatherProperties(weather_table)
-    weather_forecast.outputWeatherProperties()
+    try:
+        weather_table = weather_data.body.contents[5].contents[5].contents[22].contents[5].contents[5].table
+        weather_forecast = Weather_Forecast(lat, lng)
+        weather_forecast.detWeatherProperties(weather_table)
+        weather_forecast.outputWeatherProperties()
+        print url
+    except:
+        print 'url down'
 
 
-
+"""Main Method"""
 def main():
-    lat = 35
-    lng = -90
-    for i in range(20):
+    lat_lng = getAllCoordinates()
+    for lat, lng in lat_lng.items():
         url = getURL(lat,lng)
         getWeather(url, lat, lng)
-        lat+=.1
-        lng-=.1
 
 
 if __name__ == '__main__':
--- Weather_Forecast.py
+++ Weather_Forecast.py
@@ -1,5 +1,4 @@
 class Weather_Forecast():
-
     def __init__(self, lat, lng):
         self.humidity = ''
         self.wind_speed = ''
@@ -12,6 +11,18 @@ class Weather_Forecast():
         self.LNG = lng
 
 
+    """Determines Weather Properties.
+    @param self
+        Weather_Forecast Object
+    @param weather_table
+        HTML table containing weather data
+    @replaces self.humidity
+    @replaces self.wind_speed
+    @replaces self.wind_pressure
+    @replaces self.dew_point
+    @replaces self.visibility
+    @replaces self.heat_index
+    @replaces self.last_update"""
     def detWeatherProperties(self, weather_table):
         HUMIDITY_INDEX = 1
         WIND_SPEED_INDEX = 3
@@ -19,25 +30,34 @@ class Weather_Forecast():
         DEW_POINT_INDEX = 7
         VISIBILITY_INDEX = 9
         HEAT_INDEX_INDEX = 11
-        LAST_UPDATE_INDEX = 13
+        LAST_UPDATE_INDEX = len(weather_table.contents)-1
         self.humidity = str(weather_table.contents[HUMIDITY_INDEX].contents[3].string)
         self.wind_speed = str(weather_table.contents[WIND_SPEED_INDEX].contents[3].string)
         self.wind_pressure = str(weather_table.contents[WIND_PRESSURE_INDEX].contents[3].string)
         self.dew_point = weather_table.contents[DEW_POINT_INDEX].contents[3].string.encode('utf-8')
         self.visibility = str(weather_table.contents[VISIBILITY_INDEX].contents[3].string)
-        self.heat_index = weather_table.contents[HEAT_INDEX_INDEX].contents[1].string.encode('utf-8')
-        print weather_table.contents
-        print len(weather_table.contents)
-        self.last_update = str(weather_table.contents[LAST_UPDATE_INDEX].contents[3].string).lstrip().rstrip()
+        try:
+            self.heat_index = weather_table.contents[HEAT_INDEX_INDEX].contents[1].string.encode('utf-8')
+        except:
+            print 'No Heat'
+        try:
+            self.last_update = str(weather_table.contents[LAST_UPDATE_INDEX].contents[3].string).lstrip().rstrip()
+        except:
+            self.last_update = str(weather_table.contents[LAST_UPDATE_INDEX].string).lstrip().rstrip()
 
+
+    """Writes Weather Properties to text file
+    @param self
+        Weather Forecast Object"""
     def outputWeatherProperties(self):
-        print self.LAT
-        print self.LNG
-        print 'Humidity: ' + self.humidity
-        print 'Wind Speed: ' + self.wind_speed
-        print 'Wind Pressure: ' + self.wind_pressure
-        print 'Dew Point: ' + self.dew_point
-        print 'Visibility: ' + self.visibility
-        print 'Heat Index: ' + self.heat_index
-        print 'Last Updated: ' + self.last_update
-        print '*' * 50
+        out = open('weather_out', 'a')
+        out.write(' LAT: ' + self.LAT)
+        out.write(' LNG: ' + self.LNG)
+        out.write(' Humidity: ' + self.humidity)
+        out.write(' Wind Speed: ' + self.wind_speed)
+        out.write(' Wind Pressure: ' + self.wind_pressure)
+        out.write(' Dew Point: ' + self.dew_point)
+        out.write(' Visibility: ' + self.visibility)
+        out.write(' Heat Index: ' + self.heat_index)
+        out.write(' Last Updated: ' + self.last_update + '\n')
+        out.write('*' * 50 + '\n')
