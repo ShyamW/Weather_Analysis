213eb7af371cbdae1f254267d53a619a2a4a8d50
fire <shyam.thiagarajan@gmail.com>
2016-08-10 21:16:00 -0400

    ADDED STARTING ANALYSIS

---
+++ Analyze_Weather.py
@@ -0,0 +1,53 @@
+import plotly.plotly as py
+import pandas as pd
+
+py.sign_in('gingerbread123', '5wi0st6l25')
+df = pd.read_csv('https://raw.githubusercontent.com/plotly/datasets/master/2011_us_ag_exports.csv')
+
+for col in df.columns:
+    df[col] = df[col].astype(str)
+
+scl = [[0.0, 'rgb(242,240,247)'],[0.2, 'rgb(218,218,235)'],[0.4, 'rgb(188,189,220)'],\
+            [0.6, 'rgb(158,154,200)'],[0.8, 'rgb(117,107,177)'],[1.0, 'rgb(84,39,143)']]
+
+df['text'] = df['state'] + '<br>' +\
+    'Beef '+df['beef']+' Dairy '+df['dairy']+'<br>'+\
+    'Fruits '+df['total fruits']+' Veggies ' + df['total veggies']+'<br>'+\
+    'Wheat '+df['wheat']+' Corn '+df['corn']
+
+
+a = []
+for i in range(50):
+    a.append(i)
+data = [ dict(
+        type='choropleth',
+        colorscale = scl,
+        autocolorscale = False,
+        locations = df['code'],
+        z = a,
+        locationmode = 'USA-states',
+        text = a,
+        marker = dict(
+            line = dict (
+                color = 'rgb(255,255,255)',
+                width = 2
+            )
+        ),
+        colorbar = dict(
+            title = "Millions USD"
+        )
+    ) ]
+
+layout = dict(
+        title = '2011 US Agriculture Exports by State<br>(Hover for breakdown)',
+        geo = dict(
+            scope='usa',
+            projection=dict( type='albers usa' ),
+            showlakes = True,
+            lakecolor = 'rgb(255, 255, 255)',
+        ),
+    )
+
+fig = dict( data=data, layout=layout )
+
+url = py.plot( fig, filename='d3-cloropleth-map' )
